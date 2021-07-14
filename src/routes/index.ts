d3bf9952421e0587b8928d9bbe2a0693f757ad3a
mabutalid <theoaguilar123@gmail.com>
2021-07-14 21:34:23 +0800

    added 1 more health check

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,31 @@
 import { FastifyPluginAsync } from 'fastify';
 
 const healthCheck: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/health', async function (_request, _reply) {
-    return { status: 'Active', healthStatus: 'Healthy', service: 'be-fastify-template' };
+  fastify.decorateRequest('service', 'be-fastify-template');
+
+  fastify.get('/health', async function (req: any, _reply) {
+    return { 
+      api: 'Primary Health Check', 
+      status: 'Active', 
+      healthStatus: 'Healthy', 
+      service: req.service,
+      secondary: req.secondary
+    };
   });
+
+  fastify.register(async function innerContext (innerFastify) {
+    innerFastify.decorateRequest('secondary', 'secondary context')
+    
+    innerFastify.get('/health/secondary', async function (req: any, _reply) {
+      return { 
+        api: 'Secondary Health Check', 
+        status: 'Active', 
+        healthStatus: 'Healthy', 
+        service: req.service,
+        secondary: req.secondary
+      };
+    })
+  })
 };
 
 export default healthCheck;
